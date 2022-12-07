011cd99d4240426340ac86214f604f7b4e0881ff
naymur92 <naymurrahman@yahoo.com>
2022-12-07 22:13:57 +0600

    first commit

---
+++ src/app/admin/employee.model.ts
@@ -0,0 +1,7 @@
+export class Employee{
+  id?: string | undefined;
+  name?: string | undefined;
+  email?: string | undefined;
+  password?: string | undefined;
+  employee_type?: string | undefined;
+}
